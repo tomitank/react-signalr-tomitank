24990d9df1afb5a7bae3978395c0c5d4336374d5
Kuzmics, Tamas (G/PI) <tamas.kuzmics@audi.hu>
2025-04-01 17:24:41 +0200

    better code

--- signalR.tsx
+++ signalR.tsx
@@ -9,13 +9,12 @@
 import { AuthStates, useAuthContext } from "./auth";
 import { createContext, useContext, useEffect, useState } from "react";
 import { ToastLifeTime, useErrorHandler } from "src/hooks/errorHandler";
-import { HubConnection, HubConnectionBuilder, HubConnectionState, IHttpConnectionOptions } from "@microsoft/signalr";
+import { HubConnection, HubConnectionBuilder, HubConnectionState, IHttpConnectionOptions, IStreamResult } from "@microsoft/signalr";
 
 type ProviderProps = {
     url: string;
     children: React.ReactNode;
     options: IHttpConnectionOptions;
-    eventProvider: ({children, connection}: {children: React.ReactNode, connection: HubConnection|null}) => JSX.Element;
 };
 
 export type SignalRContextProps = {
@@ -40,31 +39,24 @@ export const useSignalRContext = () => {
  * @info reconnect is infinity
  * @requires AuthProvider
  * @returns Provider
- * @example <SignalRProvider url={axiosInstance.defaults.baseURL+'/hub'} options={{withCredentials:true}} eventProvider={SocketEventProvider}>
+ * @example <SignalRProvider url={axiosInstance.defaults.baseURL+'/hub'} options={{withCredentials:true}}>
  * <></>
  * </SignalRProvider>
  */
-export const SignalRProvider = ({children, url, options, eventProvider}: ProviderProps) => {
+export const SignalRProvider = ({children, url, options}: ProviderProps) => {
     const errorHandler = useErrorHandler();
     const {authState, userDetails} = useAuthContext();
     const [connection, setConnection] = useState<HubConnection|null>(null);
 
     const updateConnection = function(this: HubConnection) { // hack by (Tamas Kuzmics^^) to update connection states..
-        setConnection(prev => {
-            const newConnection = Object.create(this);
-            // this solved inside eventHandler -> we call .off(method) before .on(method)
-            /*if (this.state === HubConnectionState.Connected) {
-                newConnection._methods = []; // hack: clear after (re)connection, bacuse of methods fn refrerences droped..
-            }*/
-            return newConnection;
-        });
+        setConnection(prev => Object.create(this));
     };
 
     useEffect(() => {
         const _connection = new HubConnectionBuilder()
         .withUrl(url, options).withAutomaticReconnect({
-            nextRetryDelayInMilliseconds: (retryContext) => { // infinity reconnection against disconnect
-                return Math.random() * 10000; // wait between 0 and 10 seconds before the next reconnect attempt.
+            nextRetryDelayInMilliseconds: (retryContext) => { // infinity reconnection against disconnect..
+                return Math.random() * 10000; // wait between 0 and 10 seconds before the next reconnect attempt
             }
         }).build();
 
@@ -72,29 +64,30 @@ export const SignalRProvider = ({children, url, options, eventProvider}: Provide
         _connection.onreconnected(updateConnection);
         _connection.onreconnecting(updateConnection);
 
-        // overwrite send and invoke to catch error
+        // overwrite functions to keep _connection as "this" and add handle error..
+        const origOn = _connection.on;
+        const origOff = _connection.off;
         const origSend = _connection.send;
         const origInvoke = _connection.invoke;
-        _connection.send = function(this: any, methodName: string, ...args: any[]) {
-            return origSend.call(this, methodName, ...args).catch(errorHandler) as Promise<void>;
-        };
-        _connection.invoke = function<T = any>(this: any, methodName: string, ...args: any[]) {
-            return origInvoke.call(this, methodName, ...args).catch(errorHandler) as Promise<T>;
-        };
-
-        //@ts-ignore Hack: copy old methods after unmount..
-        connection !== null && (_connection._methods = connection._methods);
-        setConnection(_connection); // save
+        const origStream = _connection.stream;
+        _connection.on = (methodName: string, newMethod: (...args: any[]) => void) => origOn.call(_connection, methodName, newMethod);
+        _connection.off = (methodName: string, method?: (...args: any[]) => void) => origOff.call(_connection, methodName, method!);
+        _connection.send = (methodName: string, ...args: any[]) => origSend.call(_connection, methodName, ...args).catch(errorHandler) as Promise<void>;
+        _connection.invoke = <T extends any>(methodName: string, ...args: any[]) => origInvoke.call(_connection, methodName, ...args).catch(errorHandler) as Promise<T>;
+        _connection.stream = <T extends any>(methodName: string, ...args: any[]) => origStream.call(_connection, methodName, ...args) as IStreamResult<T>;
 
         if (authState === AuthStates.Done && userDetails)
         {
             if (_connection.state === HubConnectionState.Disconnected) {
-                _connection.start().then(() => {
-                    updateConnection.call(_connection);
-                }).catch((reason) => {
-                    updateConnection.call(_connection);
-                    errorHandler({message: `Socket error! ${reason}`, lifeTime: ToastLifeTime.OneDay});
-                });
+                setTimeout(()=> { // hack: wait disconnected state after unmount..
+                    setConnection(_connection);
+                    _connection.start().then(() => {
+                        updateConnection.call(_connection);
+                    }).catch((reason) => {
+                        updateConnection.call(_connection);
+                        errorHandler({message: `Socket error! ${reason}`, lifeTime: ToastLifeTime.OneDay});
+                    });
+                }, 0);
             }
         }
 
@@ -105,7 +98,7 @@ export const SignalRProvider = ({children, url, options, eventProvider}: Provide
 
     return (
         <SignalRContext.Provider value={{connection}}>
-            {eventProvider({children, connection})}
+            {children}
         </SignalRContext.Provider>
     );
 };
